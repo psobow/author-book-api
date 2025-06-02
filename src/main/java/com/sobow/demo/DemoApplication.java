f0e6acd985b88232a1931935a4e9a41956d42126
Patryk Sobow <patryk.sobow95@gmail.com>
2025-06-02 14:43:20 +0200

    Create Bean for JDBCTemplate in Config class

--- src/main/java/com/sobow/demo/DemoApplication.java
+++ src/main/java/com/sobow/demo/DemoApplication.java
@@ -1,30 +1,14 @@
 package com.sobow.demo;
 
-import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @Slf4j
-public class DemoApplication implements CommandLineRunner {
-    
-    private DataSource dataSource;
-    
-    public DemoApplication(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+public class DemoApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-    
-    @Override
-    public void run(String... args) throws Exception {
-        log.info("DataSource: " + dataSource.toString());
-        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        jdbcTemplate.execute("select 1");
     }
 }
